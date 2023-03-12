3db19eb7491864004831174b69ecffae585d98cd
TZGyn <ca538901d@mozmail.com>
2023-03-12 22:21:13 +0800

    Added tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+	tailwindcss: {
+		config: {
+			content: [],
+			important: false,
+			theme: {
+				extend: {
+					colors: {
+						primary: '#0c0c0d',
+						secondary: '#18181b',
+						darkgray: '#121213',
+						lightgray: '#27272a',
+						white: '#e5e7eb',
+					},
+				},
+			},
+		},
+	},
+	typescript: {
+		strict: true,
+	},
+})
