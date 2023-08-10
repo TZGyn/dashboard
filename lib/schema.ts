263a08319b543794cde2309f0d04710456cd03ee
TZGyn <ca538901d@mozmail.com>
2023-08-10 23:05:52 +0800

    Update database url to use .env

--- lib/schema.ts
+++ lib/schema.ts
@@ -11,7 +11,8 @@ import {
 } from 'drizzle-orm/pg-core'
 
 const client = postgres(
-	'postgres://postgres:password@127.0.0.1:5432/next-dashboard'
+	process.env.DATABASE_URL ??
+		'postgres://postgres:password@127.0.0.1:5432/next-dashboard'
 )
 
 export const db = drizzle(client)
