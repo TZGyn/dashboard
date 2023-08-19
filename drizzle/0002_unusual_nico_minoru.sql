ed80df0508ab69d9e7ab55d8ab235021e7cdd5a0
TZGyn <ca538901d@mozmail.com>
2023-08-19 18:58:42 +0800

    Added user + auth table

---
+++ drizzle/0002_unusual_nico_minoru.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS "session" (
+	"sessionToken" text PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"expires" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text,
+	"password" text,
+	"created_at" timestamp,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "bookmarks" ADD COLUMN "user_id" integer;--> statement-breakpoint
+ALTER TABLE "bookmark_categories" ADD COLUMN "user_id" integer;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "bookmark_categories" ADD CONSTRAINT "bookmark_categories_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "session" ADD CONSTRAINT "session_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- lib/schema.ts
+++ lib/schema.ts
@@ -1,29 +1,23 @@
 import { relations } from 'drizzle-orm'
-import {
-	pgTable,
-	serial,
-	text,
-	varchar,
-	timestamp,
-	integer,
-} from 'drizzle-orm/pg-core'
+import { pgTable, serial, text, timestamp, integer } from 'drizzle-orm/pg-core'
 
-// export const user = pgTable('users', {
-// 	id: serial('id').primaryKey(),
-// 	name: text('name'),
-// 	email: text('email'),
-// 	password: text('password'),
-// 	createdAt: timestamp('created_at'),
-// 	updatedAt: timestamp('updated_at'),
-// })
-//
-// export const userRelations = relations(user, ({ many }) => ({
-// 	bookmark: many(bookmark),
-// }))
+export const user = pgTable('users', {
+	id: serial('id').primaryKey(),
+	name: text('name'),
+	email: text('email'),
+	hashedPassword: text('password'),
+	createdAt: timestamp('created_at'),
+	updatedAt: timestamp('updated_at'),
+})
+
+export const userRelations = relations(user, ({ many }) => ({
+	bookmark: many(bookmark),
+	sessions: many(sessions),
+}))
 
 export const bookmark = pgTable('bookmarks', {
 	id: serial('id').primaryKey(),
-	// userId: integer('user_id').references(() => user.id),
+	userId: integer('user_id').references(() => user.id),
 	category_id: integer('category_id'),
 	name: text('name'),
 	link: text('link'),
@@ -32,10 +26,10 @@ export const bookmark = pgTable('bookmarks', {
 })
 
 export const bookmarkRelations = relations(bookmark, ({ one }) => ({
-	// user: one(user, {
-	// 	fields: [bookmark.userId],
-	// 	references: [user.id],
-	// }),
+	user: one(user, {
+		fields: [bookmark.userId],
+		references: [user.id],
+	}),
 	bookmarkCategory: one(bookmarkCategory, {
 		fields: [bookmark.category_id],
 		references: [bookmarkCategory.id],
@@ -44,12 +38,32 @@ export const bookmarkRelations = relations(bookmark, ({ one }) => ({
 
 export const bookmarkCategory = pgTable('bookmark_categories', {
 	id: serial('id').primaryKey(),
+	userId: integer('user_id').references(() => user.id),
 	name: text('name'),
 })
 
 export const bookmarkCategoryRelations = relations(
 	bookmarkCategory,
-	({ many }) => ({
+	({ one, many }) => ({
+		user: one(user, {
+			fields: [bookmarkCategory.userId],
+			references: [user.id],
+		}),
 		bookmark: many(bookmark),
 	})
 )
+
+export const sessions = pgTable('session', {
+	sessionToken: text('sessionToken').notNull().primaryKey(),
+	userId: integer('user_id')
+		.notNull()
+		.references(() => user.id, { onDelete: 'cascade' }),
+	expires: timestamp('expires', { mode: 'date' }).notNull(),
+})
+
+export const sessionRelations = relations(sessions, ({ one }) => ({
+	user: one(user, {
+		fields: [sessions.userId],
+		references: [user.id],
+	}),
+}))
