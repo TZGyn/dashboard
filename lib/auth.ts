35e2ab69d3b03c0ef9e37d8bfd5b4896cddf7fd3
TZGyn <ca538901d@mozmail.com>
2023-08-20 02:41:06 +0800

    Added getUser function

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
+import { db } from './db'
+
+export const getUser = async (token: RequestCookie | undefined) => {
+	if (!token) return null
+
+	const existing_session = await db.query.sessions.findFirst({
+		where: (session, { eq }) => eq(session.sessionToken, token.value),
+		with: {
+			user: true,
+		},
+	})
+
+	if (!existing_session) return null
+
+	return existing_session.user
+}
